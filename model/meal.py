307292e39c9ecd74ff723203ed98a0bb068fe2af
James Izzard <jamesaizzard@gmail.com>
2022-02-19 08:09:56 +0000

    Adds Ingredient class stub.

--- model/meal.py
+++ model/meal.py
@@ -1,2 +1,13 @@
+from typing import List
+
+from model import Ingredient
+
 class Meal():
-    pass
+    """Models a meal.
+    Meals are collections of ingredients, by ratio.
+    """
+
+    @property
+    def ingredients(self) -> List[Ingredient]:
+        """Returns the list of ingredients currently associated with the meal."""
+        raise NotImplementedError
