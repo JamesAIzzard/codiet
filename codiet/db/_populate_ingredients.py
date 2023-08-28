55ed424b5e81a2008963824ce8d864c38fdd5467
James Izzard <jamesaizzard@gmail.com>
2023-08-28 13:03:24 +0100

    Populate ingredient db with cost data from files.

--- codiet/db/_populate_ingredients.py
+++ codiet/db/_populate_ingredients.py
@@ -11,6 +11,15 @@ def _populate_ingredients(db_service: DatabaseService):
         with open(os.path.join(INGREDIENT_DATA_DIR, file)) as f:
             data = json.load(f)
 
-        # Add the ingredient name to the database
+        # Add the ingredient name to the database, getting primary key
         db_service.repo.add_ingredient_name(data['name'])
-        
+        # Get the ingredient ID from the database
+        ingredient_id = db_service.repo.get_ingredient_id(data['name'])
+        # Add the ingredient cost data
+        db_service.repo.add_ingredient_cost(
+            ingredient_id = ingredient_id,
+            cost_unit = data['cost']['cost_unit'],
+            cost_value = data['cost']['cost_value'],
+            mass_unit = data['cost']['mass_unit'],
+            mass_value = data['cost']['mass_value']
+        )
--- codiet/db/repository.py
+++ codiet/db/repository.py
@@ -2,12 +2,40 @@ class Repository:
     def __init__(self, db):
         self.db = db
 
-    def add_ingredient_name(self, name: str):
+    def get_ingredient_id(self, name: str) -> int:
+        """Gets the primary key of an ingredient from the database.
+
+        Args:
+            name (str): The name of the ingredient to get the ID of.
+
+        Returns:
+            int: The primary key of the ingredient.
+        """
+        return self.db.execute("""
+            SELECT ingredient_id FROM ingredient_base WHERE ingredient_name = ?;
+        """, (name,)).fetchone()[0]
+
+    def add_ingredient_name(self, name: str) -> int:
         """Adds a new ingredient name to the database.
 
         Args:
             name (str): The name of the ingredient to add.
         """
-        self.db.execute("""
+        return self.db.execute("""
             INSERT INTO ingredient_base (ingredient_name) VALUES (?);
         """, (name,))
+
+    def add_ingredient_cost(self, ingredient_id:int, cost_unit:str, cost_value:float, mass_unit:str, mass_value:float) -> None:
+        """Adds the cost data of an ingredient to the database.
+
+        Args:
+            ingredient_id (int): The primary key of the ingredient.
+            cost_unit (str): The unit of the cost.
+            cost_value (float): The value of the cost.
+            mass_unit (str): The unit of the mass.
+            mass_value (float): The value of the mass.
+        """
+        self.db.execute("""
+            INSERT INTO ingredient_cost (ingredient_id, cost_unit, cost_value, mass_unit, mass_value)
+            VALUES (?, ?, ?, ?, ?);
+        """, (ingredient_id, cost_unit, cost_value, mass_unit, mass_value))
