f9879e261d02b43ae9c70a9930744327fd2f955e
James Izzard <jamesaizzard@gmail.com>
2024-02-18 06:23:14 +0100

    Update UI to add/remove recipe serve times.

--- codiet/views/serve_times_editor_view.py
+++ codiet/views/serve_times_editor_view.py
@@ -1,6 +1,7 @@
 from PyQt6.QtWidgets import (
     QWidget,
     QVBoxLayout,
+    QHBoxLayout,
     QGroupBox,
     QPushButton,
     QListWidget,
@@ -11,24 +12,35 @@ class ServeTimesEditorView(QWidget):
         super().__init__()
 
         # Create vertical layout as the top level
-        layout = QVBoxLayout()
-        self.setLayout(layout)
+        lyt_top_level = QVBoxLayout()
+        self.setLayout(lyt_top_level)
         # Reduce the vertical padding in this layout
-        layout.setContentsMargins(0, 0, 0, 0)
+        lyt_top_level.setContentsMargins(0, 0, 0, 0)
 
         # Create a groupbox inside this layout
-        serve_times_group = QGroupBox("Serve Times")
-        layout.addWidget(serve_times_group)
-        serve_times_layout = QVBoxLayout()
-        serve_times_group.setLayout(serve_times_layout)
+        grp_serve_times = QGroupBox("Serve Times")
+        lyt_top_level.addWidget(grp_serve_times)
+
+        # Create a vertical layout inside the groupbox
+        lyt_serve_times = QVBoxLayout()
+        grp_serve_times.setLayout(lyt_serve_times)
+        # Reduce the vertical padding in this layout
+        lyt_serve_times.setContentsMargins(5, 5, 5, 5)
+
+        # Inside the vertical layout, create a horizontal layout for the buttons
+        lyt_buttons = QHBoxLayout()
+        lyt_serve_times.addLayout(lyt_buttons)
 
         # Create a button to add a time window
-        self.btn_add_time_window = QPushButton("Add Time Window")
-        serve_times_layout.addWidget(self.btn_add_time_window)
+        self.btn_add_time_window = QPushButton("Add")
+        lyt_buttons.addWidget(self.btn_add_time_window)
+        # Create a button to remove a time window
+        self.btn_remove_time_window = QPushButton("Remove")
+        lyt_buttons.addWidget(self.btn_remove_time_window)
 
         # Create a list widget to hold the time windows
         self.list_time_windows = QListWidget()
-        serve_times_layout.addWidget(self.list_time_windows)
+        lyt_serve_times.addWidget(self.list_time_windows)
         # Add some dummy time windows for now
         self.list_time_windows.addItem("Time Window 1")
         self.list_time_windows.addItem("Time Window 2")
