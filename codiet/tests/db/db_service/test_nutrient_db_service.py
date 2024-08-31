db609ef0eb7bfbf154d271de7938322ec6ffd244
James Izzard <jamesaizzard@gmail.com>
2024-08-31 08:05:01 +0300

    test: Draft in nutrient DB service test suite

---
+++ codiet/tests/db/db_service/test_nutrient_db_service.py
@@ -0,0 +1,29 @@
+from codiet.tests.db.database_test_case import DatabaseTestCase
+from codiet.models.nutrients.nutrient import Nutrient
+
+class TestNutrientDBService(DatabaseTestCase):
+
+    def setUp(self) -> None:
+        super().setUp()
+        
+        pass
+
+    def test_global_nutrient_id_name_map(self) -> None:
+        pass
+
+    def test_global_nutrients(self) -> None:
+        pass
+
+    def test_get_nutrient_by_name(self) -> None:
+        pass
+
+    def test_get_nutrient_by_id(self) -> None:
+        pass
+
+    def test_create_global_nutrient(self) -> None:
+        pass
+
+    def test_create_global_nutrients(self) -> None:
+        pass
+
+    
