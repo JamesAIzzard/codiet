3f1e60244f0332f9dc5aaa6cef01b49da7cf7204
James Izzard <jamesaizzard@gmail.com>
2024-09-16 18:59:51 +0100

    test: Implement improved tests on NutrientQuantity

--- codiet/model/nutrients/nutrient_quantity.py
+++ codiet/model/nutrients/nutrient_quantity.py
@@ -3,49 +3,32 @@
 from typing import TYPE_CHECKING
 
 from codiet.db.stored_entity import StoredEntity
+from codiet.model.quantity.is_quantity import IsQuantity
 from codiet.model.nutrients.nutrient import Nutrient
-from codiet.model.units.unit import Unit
 
 if TYPE_CHECKING:
     from codiet.model.ingredients.ingredient import Ingredient
 
-class NutrientQuantity(StoredEntity):
+
+class NutrientQuantity(IsQuantity, StoredEntity):
     """Class to represent the nutrient quantity associated with an entity."""
 
     def __init__(
         self,
         nutrient: Nutrient,
-        nutrient_mass_unit: Unit | None = None,
-        nutrient_mass_value: float | None = None,
         ingredient_grams_value: float | None = None,
-        *args, **kwargs
+        *args,
+        **kwargs,
     ):
         super().__init__(*args, **kwargs)
-        
+
         self._nutrient = nutrient
-        self._nutrient_mass_unit = nutrient_mass_unit
-        self._nutrient_mass_value = nutrient_mass_value
         self._ingredient_grams_value = ingredient_grams_value
 
     @property
     def nutrient(self) -> Nutrient:
         """Return the nutrient."""
         return self._nutrient
-
-    @property
-    def nutrient_mass_unit(self) -> Unit | None:
-        """Return the nutrient mass unit."""
-        return self._nutrient_mass_unit
-
-    @property
-    def nutrient_mass_value(self) -> float | None:
-        """Return the nutrient mass value."""
-        return self._nutrient_mass_value
-
-    @nutrient_mass_value.setter
-    def nutrient_mass_value(self, value: float | None):
-        """Set the nutrient mass value."""
-        self._nutrient_mass_value = value
 
     @property
     def ingredient_grams_qty(self) -> float | None:
@@ -58,19 +41,12 @@ class NutrientQuantity(StoredEntity):
         self._ingredient_grams_value = value
 
     def __hash__(self):
-        return hash(
-            (
-                self.nutrient.name,
-                self.nutrient.id,
-            )
-        )
+        return hash((self.nutrient.name))
 
     def __eq__(self, other):
         if not isinstance(other, NutrientQuantity):
             return False
 
-        if self.nutrient.id != other.nutrient.id:
-            return False
         if self.nutrient.name != other.nutrient.name:
             return False
 
---
+++ codiet/tests/model/nutrients/test_nutrient_quantity.py
@@ -0,0 +1,25 @@
+"""Tests for the NutrientQuantity class."""
+
+from codiet.tests.model import BaseModelTest
+from codiet.model.nutrients import NutrientQuantity
+
+class BaseNutrientQuantityTest(BaseModelTest):
+    """Base class for testing NutrientQuantity elements."""
+
+    def setUp(self) -> None:
+        super().setUp()
+
+class TestConstructor(BaseNutrientQuantityTest):
+    """Tests for the NutrientQuantity constructor."""
+
+    def test_minimal_arguments(self) -> None:
+        """Test the constructor with the minimum required parameters."""
+        protein = self.nutrient_fixtures.get_nutrient_by_name("protein")
+        protein_quantity = NutrientQuantity(protein)
+        self.assertIsInstance(protein_quantity, NutrientQuantity)
+
+    def test_nutrient_is_assigned(self) -> None:
+        """Test that the nutrient is assigned correctly."""
+        protein = self.nutrient_fixtures.get_nutrient_by_name("protein")
+        protein_quantity = NutrientQuantity(protein)
+        self.assertIs(protein, protein_quantity.nutrient)
