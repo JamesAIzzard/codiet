cfb6616ab10872f9dc4ddde11c7a0f9161378e8f
James Izzard <jamesaizzard@gmail.com>
2024-09-22 19:20:50 +0100

    feature/Draft in ideas around how a problem might be defined

--- codiet/tests/unit/optimisation/optimiser/test_optimiser.py
+++ codiet/tests/unit/optimisation/optimiser/test_optimiser.py
@@ -1,4 +1,5 @@
 from typing import Collection
+from unittest import skip
 
 from codiet.tests import BaseCodietTest
 from codiet.tests.fixtures.constraints import ConstraintTestFixtures
@@ -38,3 +39,35 @@ class TestSolve(BaseOptimiserTest):
         self.assertIsInstance(results, Collection)
         for item in results:
             self.assertIsInstance(item, DietPlan)
+
+    @skip("In progress")
+    def test_all_diet_plans_satisfy_flag_constraints(self):
+
+        day1 = DayProblem("Day 1")
+        day1_breakfast = MealProblem("Breakfast")
+        day1_breakfast_drink = RecipeProblem("Drink")
+        day1_breakfast_main = RecipeProblem("Main")
+        day1_breakfast.add_recipe_problem(day1_breakfast_drink)
+        day1_breakfast.add_recipe_problem(day1_breakfast_main)
+        day1_breakfast_drink.add_constraint(TagConstraint("drink"))
+        day1_breakfast_main.add_constraint(TagConstraint("main"))
+        day1_breakfast.add_constraint(FlagConstraint("vegan", True))
+        day1_breakfast.add_constraint(FlagConstraint("gluten_free", True))
+        # day1_breakfast.add_constraint(TimeConstraint("08:00"))
+        # day1_breakfast.add_goal(NutrientMassTargetGoal("protein", Quantity(40, "g")))
+        # day1_breakfast.add_goal(NutrientMassMinimiseGoal("carbohydrate")
+        # day1_breakfast.add_goal(NutrientMassMinimiseGoal("fat"))
+        # day1_breakfast.add_goal(CostMinimiseGoal())
+        # day1_breakfast.add_goal(CalorieTargetGoal(Quantity(500)))
+        day1.add_meal_problem(day1_breakfast)
+
+        optimiser = Optimiser()
+        optimiser.set_recipe_source(self.recipe_fixtures.recipes.values())
+        optimiser.set_algorithm(algorithms.Deterministic())
+        optimiser.set_problem([day1])
+
+        results = optimiser.solve()
+
+        for diet_plan in results:
+            self.assertTrue(diet_plan.get_flag('vegan').value)
+            self.assertTrue(diet_plan.get_flag('gluten_free').value)
